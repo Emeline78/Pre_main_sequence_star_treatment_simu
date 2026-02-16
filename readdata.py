45df911b2d7d81b5c45a261d1e13c3c0991ec726
localuser <localuser@e-m1-en-15>
2026-02-16 14:13:19 +0100

    modifications

--- readdata.py
+++ readdata.py
@@ -31,10 +31,10 @@ rcParams['figure.figsize']= [8, 6]
 # this is the most important line from the header - it loads the python routines to analyse data
 from magic import *
 
-my_path = '/travail/dynconv/multiscale_dyno/anelasticCouette/gr2/xi_p35_pm4/ra_2e6/om300'
+my_path = '/travail/dynconv/multiscale_dyno/anelasticCouette/gr2/xi_p35_pm4/ra_2e6/om50'
 
 s = Surf(datadir=my_path,ivar=12, ave=False, tag='rot01') # useful plotting routines ivar = 20 donne le dernier output
-print(s.__getattribute__)
+
 s.avg(field='Br', cm='seismic') # plots of averages
 s.surf('Br', r=0.9, cm = 'seismic') # surface plots
 
