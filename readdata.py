0a6f295c3887329e70ea02c376397ff9432d2edc
localuser <localuser@e-m1-en-15>
2026-02-16 11:37:08 +0100

    modifications

--- readdata.py
+++ readdata.py
@@ -31,13 +31,12 @@ rcParams['figure.figsize']= [8, 6]
 # this is the most important line from the header - it loads the python routines to analyse data
 from magic import *
 
-my_path = '/travail/dynconv/multiscale_dyno/anelasticCouette/gr2/xi_p35_pm4/ra_2e6/om100'
+my_path = '/travail/dynconv/multiscale_dyno/anelasticCouette/gr2/xi_p35_pm4/ra_2e6/om25'
 
-s = Surf(datadir=my_path,ivar=12, ave=False, tag='rot01') # useful plotting routines ivar = 20 donne le dernier output
+s = Surf(datadir=my_path,ivar=20, ave=False, tag='rot01') # useful plotting routines ivar = 20 donne le dernier output
 s.avg(field='Br', cm='seismic') # plots of averages
-s.surf(field='Br', r=0.9, cm = 'seismic') # surface plots
-
-gr = MagicGraph(datadir=my_path,ivar=12,tag='rot01') # access to 3D fields 
+s.surf('Br', r=0.9, cm = 'seismic') # surface plots
+gr = MagicGraph(datadir=my_path,ivar=20,tag='rot01') # access to 3D fields 
 
 gr.vtheta # theta component of velocity - numpy array
 print(gr.vtheta.shape) # theta component of velocity - shape
@@ -48,5 +47,5 @@ gr.ntheta # number of pts in latitude
 gr.nphi # number of pts in azimuth
 # you have to "construct" the grid in phi and theta yourself - check the plotting routines 
 
-sp = MagicSpectrum(datadir=my_path,tag='rot01', field='e_mag', ispec=12) # 1D spectra
-sp = MagicSpectrum2D(datadir=my_path,tag='rot01', field='e_mag', ispec=12) # 2D spectra - throughs an error for now, check with MagIC website
+sp = MagicSpectrum(datadir=my_path,tag='rot01', field='e_mag', ispec=20) # 1D spectra
+sp = MagicSpectrum2D(datadir=my_path,tag='rot01', field='e_mag', ispec=20) # 2D spectra - throughs an error for now, check with MagIC website
