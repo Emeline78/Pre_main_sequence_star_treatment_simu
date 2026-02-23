277df998ba2fccd61f39b7a8e236f7c81664c897
localuser <localuser@e-m1-en-15>
2026-02-23 16:07:53 +0100

    modifications

--- plot_grap_files.py
+++ plot_grap_files.py
@@ -40,6 +40,7 @@ for a in ["gr/Nr2p5_Pm4/ra_8e6/om50","gr2/xi_p2_pm4/ra_1e6/om15","gr2/xi_p35_pm4
 	
 	thlin = np.linspace(0., np.pi, gr.ntheta)
 	indices = np.where(np.isclose(thlin, np.pi*81/180))[0]
+	print(indices)
 	filtre = (gr.vr[:,indices,:]).mean(axis=0)
 	plt.figure(50)
 	plt.plot(gr.radius,filtre[0])
