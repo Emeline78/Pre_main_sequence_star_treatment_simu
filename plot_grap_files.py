3c29452c18d5d6ad3772c5f772ccebdbfa280c4d
localuser <localuser@e-m1-en-15>
2026-02-23 16:41:09 +0100

    modifications

--- plot_grap_files.py
+++ plot_grap_files.py
@@ -30,36 +30,21 @@ rcParams['figure.figsize']= [8, 6]
 
 from magic import *
 
-for i,a in enumerate(["gr/Nr2p5_Pm4/ra_8e6/om50","gr2/xi_p2_pm4/ra_1e6/om15","gr2/xi_p35_pm4/ra_5e6/om50"]):
+for a in ["gr/Nr2p5_Pm4/ra_8e6/om50","gr2/xi_p2_pm4/ra_1e6/om15","gr2/xi_p35_pm4/ra_5e6/om50"]:
 	my_path = '/travail/dynconv/multiscale_dyno/anelasticCouette/'+a
 	gr = MagicGraph(datadir=my_path,ivar=9,tag='rot01') # access to 3D fields 
 	s = Surf(datadir=my_path,ivar=9, ave=False, tag='rot01')
-
-
-	fig, axes = plt.subplots(3, 3, figsize=(15, 12))
-	for j,field in enumerate(['vr','br','helicity']):
-		ax = axes[i, j]  
-		surf_data = s.avg(field=field, cm='seismic', show=False)
-		data = surf_data.get_array()  # récupère les données 2D
-		lon, lat = surf_data.get_coords()  # récupère les coordonnées
-
-		# Tracer sur le subplot correspondant
-		im = axes[i, j].pcolormesh(lon, lat, data, cmap='seismic')
-		if i == 0:
-			ax.set_title(field, fontsize=14)
-
-plt.tight_layout()
+	s.avg(field='vr', cm='seismic')
+	s.avg(field='br', cm='seismic')
+	s.avg(field='helicity', cm='seismic')
+	
+	#thlin = np.linspace(0., np.pi, gr.ntheta)
+	#indices = np.where(np.isclose(thlin, np.pi*81/180,atol=1e-1))[0]
+	#print(indices)
+	#filtre = (gr.vr[:,indices,:]).mean(axis=0)
+	#plt.figure(50)
+	#plt.plot(gr.radius,filtre[0])
 plt.show()
-	
-	
-	
-#thlin = np.linspace(0., np.pi, gr.ntheta)
-#indices = np.where(np.isclose(thlin, np.pi*81/180,atol=1e-1))[0]
-#print(indices)
-#filtre = (gr.vr[:,indices,:]).mean(axis=0)
-#plt.figure(50)
-#plt.plot(gr.radius,filtre[0])
-#plt.show()
 
 	
 
