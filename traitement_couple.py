a9d22c9f0c1f58c69b43382fcf7d4f7a1b7d13e1
localuser <localuser@e-m1-en-15>
2026-03-16 14:39:58 +0100

    modifications

--- traitement_couple.py
+++ traitement_couple.py
@@ -31,8 +31,9 @@ g1 = MagicGraph(datadir=a,tag='rot01',ivar = 1)
 files = glob.glob(os.path.join(a, 'G_[0-9]*.rot01'))
 prod_tot = np.zeros((len(files),g1.nr))
 times = []
-print(files)
+
 for j in range(1,len(files)+1):
+	print(j)
 	gr = MagicGraph(datadir=a,tag='rot01',ivar = j)
 	times.append(gr.time)
 	r = gr.radius
@@ -49,21 +50,22 @@ for j in range(1,len(files)+1):
 
 times = np.array(times)
 dt = np.diff(times)
-print(prod_tot.shape,times.shape)
-
 t_total = times[-1] - times[0]
 
-RS = (prod_tot*dt[:, np.newaxis]).sum(axis=0) / t_total
+for i in range(len(dt)):
+	RS += 0.5 * (prod_tot[i] + prod_tot[i+1]) * dt[i]
+RS = RS / t_total
 print(RS.shape)
 plt.figure()
 plt.plot(RS)
 plt.show()
 
 #temp0, rho0, beta = anelprof(gr.radius,strat=gr.strat, polind=gr.polind,g0=gr.g0, g1=gr.g1, g2=gr.g2)
-
 #RS = rho0 * RS
 
+# -------------- Maxwell stress ---------------
 prod_tot = np.zeros((len(files),g1.nr))
+
 for j in range(1,len(files)+1):
 	gr = MagicGraph(datadir=a,tag='rot01',ivar = j)
 	r = gr.radius
@@ -77,7 +79,10 @@ for j in range(1,len(files)+1):
 	int_theta =  (int_phi*dthet[:, np.newaxis]).sum(axis = 0)
 	prod_tot[j-1] = int_theta/(4*np.pi) *r
 
-MS = (prod_tot*dt).sum(axis=0) / t_total * (-1/(4* np.pi * 1e-7))
+for i in range(len(dt)):
+	MS += 0.5 * (prod_tot[i] + prod_tot[i+1]) * dt[i]
+	
+MS = MS / t_total * (-1/(4* np.pi * 1e-7))
 print(MS.shape)
 plt.figure()
 plt.plot(MS)
