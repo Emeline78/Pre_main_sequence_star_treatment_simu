f6e0399daeb25b1463b53658e09c46a568ae1b59
localuser <localuser@e-m1-en-15>
2026-04-16 16:34:45 +0200

    modifications

--- traitement_couple.py
+++ traitement_couple.py
@@ -99,7 +99,7 @@ for j in range(1,len(files)+1):
     # Moy champ mag
     Br_mean = (gr.Br * w_phi).sum(axis=0)
     Bphi_mean = (gr.Bphi * w_phi).sum(axis=0)
-    MS1 = (Br_mean * Bphi_mean * np.sin(th)[:,None] * w_theta[:,None]).sum(axis = 0) * r
+    MS1 = -(Br_mean * Bphi_mean * np.sin(th)[:,None] * w_theta[:,None]).sum(axis = 0) * r
     
     # Ecoulement meridional
     vr_mean = (gr.vr * w_phi).sum(axis=0)
