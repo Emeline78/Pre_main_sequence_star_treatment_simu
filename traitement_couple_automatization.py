751939ac902af260494afdc1ca8f590d698d7f89
localuser <localuser@e-m1-en-15>
2026-04-20 15:37:34 +0200

    modifications

---
+++ traitement_couple_automatization.py
@@ -0,0 +1,197 @@
+import pandas as pd
+from math import *
+import numpy as np
+from scipy import *
+import matplotlib.pyplot as plt
+import glob
+import os
+import matplotlib
+matplotlib.interactive(True)
+from magic.libmagic import anelprof
+from matplotlib.ticker import ScalarFormatter
+import re
+formatter = ScalarFormatter(useMathText=True)
+formatter.set_scientific(True)
+formatter.set_powerlimits((0, 0))
+from pathlib import Path
+
+"""
+git add traitement_couple_automatization.py
+git commit -m "modifications"
+git push
+"""
+
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.size'] = 24
+matplotlib.rcParams['ytick.labelsize']=20
+matplotlib.rcParams['ytick.direction']='in'
+matplotlib.rcParams['ytick.right']='True'
+matplotlib.rcParams['xtick.labelsize']=20
+matplotlib.rcParams['xtick.direction']='in'
+matplotlib.rcParams['xtick.top']='True'
+matplotlib.rcParams['figure.figsize']= [8, 6]
+
+def extract_params(path):
+	match = re.findall(r'(gr2?|gr_gr2_Louis|Nr\d+|xi\d+|ra\d+|om\d+)', str(path))
+	params = {}
+	for m in match:
+		if m.startswith("gr"):
+			params["config"] = m
+		elif m.startswith("Nr"):
+			params["Nr"] = int(m[2:])
+		elif m.startswith("xi"):
+			params["xi"] = int(m[2:])
+		elif m.startswith("ra"):
+			params["ra"] = int(m[2:])
+		elif m.startswith("om"):
+			params["Ro_sh"] = int(m[2:])/Ek
+	return params
+
+def make_case_name(params):
+	keys = ["config","Nr","xi","ra","om"]
+	return "_".join(f"{k}{params[k]}" for k in keys if k in params)
+
+
+def save_snapshots(save_dir, case_name, r, times, RS, MS, MC, Visc):
+	save_dir.mkdir(parents=True, exist_ok=True)
+	np.savez(save_dir / f"{case_name}.npz",r=r,times=times,RS=RS,MS=MS,MC=MC,Visc=Visc)
+
+def load_snapshots(file):
+	data = np.load(file)
+	return data["r"], data["times"], data["RS"], data["MS"], data["MC"], data["Visc"]
+
+from magic import *
+mu0 = 4*np.pi*1e-7
+
+liste = []
+index = []
+snap_dir = "snapshots"
+
+all_dirs = (list(Path("/travail/dynconv/multiscale_dyno/anelasticCouette/gr").glob("Nr*/ra*/om*")) +list(Path("/travail/dynconv/multiscale_dyno/anelasticCouette/gr2").glob("xi*/ra*/om*")) +list(Path("/travail/dynconv/multiscale_dyno/anelasticCouette/gr_gr2_Louis").glob("ra*/om*")))
+
+for path in all_dirs:
+	a = str(path)
+	stp = MagicSetup(datadir = a)
+
+	n = stp.polind
+	Pm = stp.prmag
+	ki = stp.radratio
+	Nrho = stp.strat 
+	Ek = stp.ek
+	g0 = stp.g0
+	g1 = stp.g1
+	g2 = stp.g2
+	om = 1/Ek
+	
+	params = extract_params(path)
+	case_name = make_case_name(params)
+	snap_file = snap_dir / f"{case_name}.npz"
+
+	ts = MagicTs(datadir = a, field='e_kin', all=True) 	# verification que le regime ne change pas dans le temps pour pouvoir faire l'integration en temps 
+
+	files = glob.glob(os.path.join(a,'G_[0-9]*.rot01'))
+	files.sort(key=lambda f: int(os.path.basename(f).split('_')[1].split('.')[0]))
+
+	times = []
+	RS_snap = []
+	MS_snap = []
+	Visc_snap = []
+	MC_snap = []
+	l_snap = []
+
+	# pour les moyennes sur phi j'aurais juste pu faire mean vu que c'est espace regulierement
+	for j in range(1,len(files)+1): 
+		gr = MagicGraph(datadir=a,tag='rot01',ivar = j)
+		times.append(gr.time)
+
+		if j == 1:
+			r = gr.radius
+			th = np.linspace(0,np.pi,gr.ntheta)
+			phi = np.linspace(0,2*np.pi,gr.nphi-1)
+
+			dphi = 2*np.pi/(gr.nphi-2)
+			dtheta = np.pi/(gr.ntheta-1)
+
+			w_theta = dtheta * np.sin(th)
+			w_phi = dphi / (2* np.pi)
+
+		# fluctuations
+		vr = gr.vr - gr.vr.mean(axis=0)
+		vp = gr.vphi - gr.vphi.mean(axis=0)
+
+		# def de tau
+		dvphi = np.gradient(gr.vphi, r, axis=2)
+		tau_rphi = dvphi - gr.vphi/r[None,None,:] 
+
+		# Reynolds
+		prodR = (vr * vp * w_phi).sum(axis=0)	# flux
+		RS = (prodR * np.sin(th)[:,None] * w_theta[:,None]).sum(axis=0) * r # integrated flux over a spherical surface
+
+		# Maxwell
+		prodM = -(gr.Br * gr.Bphi * w_phi).sum(axis=0)
+		MS = (prodM * np.sin(th)[:,None] * w_theta[:,None]).sum(axis=0) * r  
+
+		# Ecoulement meridional
+		vr_mean = (gr.vr * w_phi).sum(axis=0)
+		vphi_mean = (gr.vphi * w_phi).sum(axis=0)
+		MC = (vr_mean * (vphi_mean + r[None,:] * np.sin(th)[:,None] * 1/Ek) * np.sin(th)[:,None] * w_theta[:,None]).sum(axis = 0) * r
+
+		# Viscosite
+		mean_tau = (tau_rphi * w_phi).sum(axis = 0)
+		Visc = - (mean_tau * np.sin(th)[:,None] * w_theta[:,None]).sum(axis=0) * r
+
+		# moment angulaire
+		l = (r[None,None,:]*np.sin(th)[:,None]**2*gr.vphi).mean(axis = (0,1)) * 2 * np.pi * r**2
+		l_snap.append(l)
+
+		Visc_snap.append(Visc)
+		RS_snap.append(RS)
+		MS_snap.append(MS)
+		MC_snap.append(MC)
+
+	times = np.array(times)
+
+	RS_snap = np.array(RS_snap)
+	MS_snap = np.array(MS_snap)
+	Visc_snap = np.array(Visc_snap)
+	MC_snap = np.array(MC_snap)
+	l_snap = np.array(l_snap)
+	
+	save_snapshots(snap_dir,case_name,r,times,RS_snap,MS_snap,MC_snap,Visc_snap)
+
+	t_total = times[-1] - times[0]
+	dt = np.diff(times)
+
+	RS = np.zeros_like(RS_snap[0])
+	MS = np.zeros_like(MS_snap[0])
+	MC = np.zeros_like(MC_snap[0])
+	Visc = np.zeros_like(Visc_snap[0])
+	for i in range(len(dt)):
+		RS += 0.5*(RS_snap[i] + RS_snap[i+1])*dt[i]
+		MS += 0.5*(MS_snap[i] + MS_snap[i+1])*dt[i]
+		MC += 0.5*(MC_snap[i] + MC_snap[i+1])*dt[i]
+		Visc += 0.5*(Visc_snap[i] + Visc_snap[i+1])*dt[i]
+
+	L = 1		# pas 1 - ki car r0 n'est pas egale a 1 mais a 1/(1-ki)
+	nu = Ek * om * L**2
+	tau = L**2/nu		# savoir quoi prendre entre temps visqueux (L**2/nu) ou de rotation (1/om)
+	eta = nu/Pm
+	temp, rho, drho = anelprof(r, strat = Nrho, polind = n, g0=g0, g1=g1, g2=g2)
+	rho0 = rho[0]
+	rho = rho / rho0  
+	B0car = eta * om * mu0 	* rho0
+
+	RS = RS / t_total * rho * L**3 / tau**2 * 2 * np.pi * r**2
+	MS = MS / t_total * L * B0car / mu0 * 2 * np.pi * r**2
+	Visc = Visc / t_total * rho * L**3 / tau**2 * 2 * np.pi * r**2
+	MC = MC / t_total * rho * L**3 / tau**2 * 2 * np.pi * r**2
+
+	params = extract_params(path)
+	res = pd.DataFrame({"r": r,"RS": RS, "MC": MC, "MS": MS, "Visc": Visc})
+	for key, value in params.items():
+        	res[key] = value
+	liste.append(res)
+	
+df_final = pd.concat(liste, ignore_index=True)
+df_final.to_parquet("transport_profiles.parquet")
