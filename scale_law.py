f0ebba11ee93027a099b7a0d9e3b45235428ca7a
localuser <localuser@e-m1-en-15>
2026-04-21 11:41:31 +0200

    modifications

---
+++ scale_law.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+git add scale_law.py
+git commit -m "modifications"
+git push
+"""
+
+df = pd.read_parquet("transport_profiles.parquet")
+
+MS_mean = (df.groupby("name")["MS"].mean()).to_numpy()
+names = (df.groupby("name")["name"].unique()).to_numpy()
+
+Ra = (df.groupby("name")["Ra"].unique()).to_numpy()
+g = (df.groupby("name")["config_code"].unique()).to_numpy()
+Ro_sh = (df.groupby("name")["om"].unique()).to_numpy() / 1e-4
+
+MS_min = np.full(len(names),np.nan)
+MS_max = np.full(len(names),np.nan)
+for i,a in enumerate(names): 
+	data = np.load(file)
+	MS_snap = data["MS"]
+	MS_min[i] = np.min(np.mean(MS_snaps,axis = 1))
+	MS_max[i] = np.max(np.mean(MS_snaps,axis = 1))
+
+plt.figure()
+plt.errorbar(Ra, MS_mean, yerr=[MS_min, MS_max], fmt='o')
+plt.xlabel("Rayleigh number")
+plt.ylabel("Radial mean of the Maxwell stress of each run")
+
+plt.figure()
+plt.errorbar(g, MS_mean, yerr=[MS_min, MS_max], fmt='o')
+plt.xlabel("Gravity configuration")
+plt.ylabel("Radial mean of the Maxwell stress of each run")
+
+plt.figure()
+plt.errorbar(Ro_sh, MS_mean, yerr=[MS_min, MS_max], fmt='o')
+plt.xlabel("Rossby shear number")
+plt.ylabel("Radial mean of the Maxwell stress of each run")
+
+plt.figure()
+plt.scatter(Ra, g, c=MS_mean, cmap='viridis')
+plt.colorbar(label='Radial mean of the Maxwell stress of each run')
+plt.xlabel('Rayleigh number')
+plt.ylabel('Gravity configuration')
+
+plt.figure()
+plt.scatter(Ra, Ro_sh, c=MS_mean, cmap='viridis')
+plt.colorbar(label='Radial mean of the Maxwell stress of each run')
+plt.xlabel('Rayleigh number')
+plt.ylabel('Rossby shear number')
+	
