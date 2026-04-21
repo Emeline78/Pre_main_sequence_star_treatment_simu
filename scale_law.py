b49d902507e176ea86b1c3fbba3fcd388a2ffc88
localuser <localuser@e-m1-en-15>
2026-04-21 17:44:53 +0200

    modifications

--- scale_law.py
+++ scale_law.py
@@ -32,12 +32,20 @@ MS_max_dist = np.full(len(names),np.nan)
 for i,namefile in enumerate(names): 
 	data = np.load("snapshots/"+namefile+".npz")
 	MS_snap = data["MS"]
-	x = np.mean(MS_snap,axis = 1)
-	print("df mean:", MS_mean[i])
-	print("snap mean(radial then time):", np.mean(np.mean(MS_snap,axis=1)))
+	times = data["times"]
+
+	MS_snap_mean = np.trapz(np.mean(MS_snap, axis=1), times) / (times[-1]-times[0])
+	print(MS_snap_mean,MS_mean[i])
+	"""
+	plt.figure()
+	plt.plot(x)
+	plt.axhline(MS_mean[i], color='r', linestyle='--')
+	plt.show()
+	"""
 	MS_mean_dist[i] = np.sqrt(np.mean((x - MS_mean[i])**2)) / np.sqrt(len(x))
 	
 	x = np.max(np.abs(MS_snap),axis = 1)
+	print(np.mean(x),MS_max[i])
 	MS_max_dist[i] = np.sqrt(np.mean((x - MS_max[i])**2)) / np.sqrt(len(x))
 
 plt.figure()
