06f0309e9e2f905bfbce6f48c769a7846ebac87f
localuser <localuser@e-m1-en-15>
2026-05-04 15:24:07 +0200

    modifications

--- scale_law.py
+++ scale_law.py
@@ -50,24 +50,7 @@ for i,namefile in enumerate(names):
 	#print(np.mean(x),MS_max[i])
 	MS_max_dist[i] = np.sqrt(np.mean((x - MS_max[i])**2)) / np.sqrt(len(x))
 
-# ===================== OBSERVATIONAL DATA =====================
 
-#B_obs_kG = np.array([])   # kG
-Prot_days = np.array([11.55,0.91,0.79,3.70,3.48,11.00]) # days
-tau_conv_days = np.array([268.3,363.1,357.9,351.9,238.0,334.1]) # days
-
-# --- Unit conversions ---
-#B_obs = B_obs_kG * 1e3 * 1e-4  # Tesla
-Omega_obs = 2 * np.pi / (Prot_days * 86400)
-
-# --- ASSUMPTIONS ---
-mu0 = 4 * np.pi * 1e-7
-
-rho = 100     # kg/m^3 (take the ones for the right age of Cesam)
-eta = 1e8     # m^2/s (hope for the best)
-
-#Els_obs = B_obs**2 / (rho * mu0 * eta * Omega_obs)
-Ro_conv_obs = Prot_days / tau_conv_days
 
 # ================== PLOTS ================================
 cmap1 = mcolors.ListedColormap(cm.inferno([0.2, 0.55, 0.9]))
@@ -242,25 +225,20 @@ def interp(x,y,yerr):
 
 # ======================== ROSSBY CONVECTIF =========================
 a_mean,b_mean,x_plot,y_plot = interp(Ro_conv[mask],MS_mean[mask],MS_mean_dist[mask])
-MS_obs = 10**b_mean * Ro_conv_obs**a_mean
 
 plt.figure()
 plt.subplot(1,2,1)
 plt.plot(x_plot, y_plot, color='orange')
 plt.errorbar(Ro_conv[mask],MS_mean[mask], yerr=MS_mean_dist[mask], fmt='o', color='black')
-plt.scatter(Ro_conv_obs, MS_obs, marker='*', s=120, color='red', edgecolor='black',label="Observations")
 plt.xlabel("Convective Rossby")
 plt.ylabel("MS mean")
 plt.grid()
 plt.title(rf"$MS_{{mean}} = 10^{{{b_mean:.2f}}} \cdot Ro_{{conv}}^{{{a_mean:.2f}}}$")
 
 a_max,b_max,x_plot,y_plot = interp(Ro_conv[mask],MS_max[mask],MS_max_dist[mask])
-MS_obs = 10**b_max * Ro_conv_obs**a_max
-
 plt.subplot(1,2,2)
 plt.plot(x_plot, y_plot, color='orange')
 plt.errorbar(Ro_conv[mask],MS_max[mask], yerr=MS_max_dist[mask], fmt='o', color='black')
-plt.scatter(Ro_conv_obs, MS_obs, marker='*', s=120, color='red', edgecolor='black',label="Observations")
 plt.xlabel("Convective Rossby")
 plt.ylabel("MS max")
 plt.title(rf"$MS_{{max}} = 10^{{{b_max:.2f}}} \cdot Ro_{{conv}}^{{{a_max:.2f}}}$")
@@ -309,7 +287,28 @@ plt.title(rf"$MS_{{max}} = 10^{{{b_max:.2f}}} \cdot Rm^{{{a_max:.2f}}}$")
 plt.grid()
 plt.show()
 
-"""
+
+# ===================== OBSERVATIONAL DATA =====================
+
+B_obs_kG = np.array([1,3])   # kG
+Prot_days = np.array([11.55,0.91,0.79,3.70,3.48,11.00]) # days
+tau_conv_days = np.array([268.3,363.1,357.9,351.9,238.0,334.1]) # days
+
+# --- Unit conversions ---
+B_obs = B_obs_kG * 1e3 * 1e-4  # Tesla
+Omega_obs = 2 * np.pi / (Prot_days * 86400)
+
+# --- ASSUMPTIONS ---
+mu0 = 4 * np.pi * 1e-7
+
+rho = 100     # kg/m^3 (take the ones for the right age of Cesam)
+eta = 1e8     # m^2/s (hope for the best)
+
+#Els_obs = B_obs**2 / (rho * mu0 * eta * Omega_obs)
+Ro_conv_obs = Prot_days / tau_conv_days
+
+MS_obs_lim = B_obs**2/mu0
+
 #=======================================================================================================
 df_tot = pd.read_parquet("transport_profiles_SI.parquet")
 for date, df in df_tot.groupby('date'):
@@ -318,7 +317,8 @@ for date, df in df_tot.groupby('date'):
 	MS_max = df.groupby("name")["MS_SI"].apply(lambda x: x.iloc[x.abs().argmax()]).to_numpy()
 
 	names = df.groupby("name").mean().index.to_numpy(dtype = str)
-
+	
+	scale = (df.groupby("name")["scale"].first()).to_numpy()
 	Ra = (df.groupby("name")["ra"].first()).to_numpy()
 	g = (df.groupby("name")["config_code"].first()).to_numpy()
 	om = (df.groupby("name")["om"].first()).to_numpy()
@@ -331,22 +331,17 @@ for date, df in df_tot.groupby('date'):
 	mask = (om < om_lim) & (df.groupby("name")["status"].first().to_numpy()) & (np.char.find(names, "wrong") == -1)
 	#mask = (om > om_lim) & (df.groupby("name")["status"].first().to_numpy()) & (np.char.find(names, "wrong") == -1)
 
-	r_phys = 1e9
-	Omega_phys = 1e-6
-	rho_ref = 1e-7
-	mu0 = 4*np.pi*1e-7
-	L_phys = r_phys * (1-(df.groupby("name")["xi"].first()).to_numpy())
-	scale = rho_ref * Omega_phys**2 * L_phys**5
 
 	MS_mean_dist = np.full(len(names),np.nan)
 	MS_max_dist = np.full(len(names),np.nan)
 	for i,namefile in enumerate(names): 
+		sim = df[df["name"] == namefile]
+		r_phys = sim["r_phys"].values
 		data = np.load("snapshots/"+namefile+".npz")
 		MS_snap = data["MS"] 
 		times = data["times"]
-		r = data["r"]
 		
-		x = np.mean(MS_snap, axis=1)* scale[i]
+		x = np.mean(MS_snap, axis=1)* scale[i] / r_phys**3
 		MS_snap_mean = np.trapz(x, times) / (times[-1]-times[0])
 		#print(MS_snap_mean,MS_mean[i])
 		MS_mean_dist[i] = np.sqrt(np.mean((x - MS_mean[i])**2)) / np.sqrt(len(x))
@@ -356,129 +351,18 @@ for date, df in df_tot.groupby('date'):
 		MS_max_dist[i] = np.sqrt(np.mean((x - MS_max[i])**2)) / np.sqrt(len(x))
 
 
-	cmap1 = mcolors.ListedColormap(cm.inferno([0.2, 0.55, 0.9]))
-	cmap2 = mcolors.ListedColormap(cm.cool([0.1, 0.5, 0.9]))
-
-	norm_discrete = mcolors.BoundaryNorm(boundaries=[-0.5, 0.5, 1.5, 2.5], ncolors=3)
-	color_values = g[mask]
-
-	g_labels = {0: r"$g \propto r$", 1: r"$g \propto 1/r^2$", 2: "CESAM 2k20"}
-
-	def add_colorbar(sc):
-	    cb = plt.colorbar(sc, ticks=[0, 1, 2])
-	    cb.ax.set_yticklabels([g_labels[0], g_labels[1], g_labels[2]])
-
-	colors1 = cmap1(norm_discrete(color_values))
-	colors2 = cmap2(norm_discrete(color_values))
-
-	# --- Figure 1 : Rossby convectif ---
-	plt.figure()
-	plt.subplot(1,2,1)
-	plt.errorbar(Ro_conv[mask], MS_mean[mask], yerr=MS_mean_dist[mask], fmt='none', ecolor=colors1)
-	sc = plt.scatter(Ro_conv[mask], MS_mean[mask], c=color_values, cmap=cmap1, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Rossby convectif")
-	plt.ylabel("MS mean")
-	plt.title("Radial mean of MS as a function of the convective Rossby")
-	plt.grid()
-
-	plt.subplot(1,2,2)
-	plt.errorbar(Ro_conv[mask], MS_max[mask], yerr=MS_max_dist[mask], fmt='none', ecolor=colors2)
-	sc = plt.scatter(Ro_conv[mask], MS_max[mask], c=color_values, cmap=cmap2, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Rossby convectif")
-	plt.ylabel("MS max")
-	plt.title("Radial max of MS as a function of the convective Rossby")
-	plt.grid()
-
-	# --- Figure 2 : Elsasser ---
-	plt.figure()
-	plt.subplot(1,2,1)
-	plt.errorbar(Els[mask], MS_mean[mask], yerr=MS_mean_dist[mask], fmt='none', ecolor=colors1)
-	sc = plt.scatter(Els[mask], MS_mean[mask], c=color_values, cmap=cmap1, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Elsasser number")
-	plt.ylabel("MS mean")
-	plt.title("Radial mean of MS as a function of the Elsasser number")
-	plt.grid()
-
-	plt.subplot(1,2,2)
-	plt.errorbar(Els[mask], MS_max[mask], yerr=MS_max_dist[mask], fmt='none', ecolor=colors2)
-	sc = plt.scatter(Els[mask], MS_max[mask], c=color_values, cmap=cmap2, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Elsasser number")
-	plt.ylabel("MS max")
-	plt.title("Radial max of MS as a function of the Elsasser number")
-	plt.grid()
-
-	# --- Figure 3 : Rossby shear ---
-	plt.figure()
-	plt.subplot(1,2,1)
-	plt.errorbar(Ro_sh[mask], MS_mean[mask], yerr=MS_mean_dist[mask], fmt='none', ecolor=colors1)
-	sc = plt.scatter(Ro_sh[mask], MS_mean[mask], c=color_values, cmap=cmap1, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Rossby shear")
-	plt.ylabel("MS mean")
-	plt.title("Radial mean of MS as a function of the Rossby shear")
-	plt.grid()
-
-	plt.subplot(1,2,2)
-	plt.errorbar(Ro_sh[mask], MS_max[mask], yerr=MS_max_dist[mask], fmt='none', ecolor=colors2)
-	sc = plt.scatter(Ro_sh[mask], MS_max[mask], c=color_values, cmap=cmap2, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Rossby shear")
-	plt.ylabel("MS max")
-	plt.title("Radial max of MS as a function of the Rossby shear")
-	plt.grid()
-
-	# --- Figure 4 : Reynolds magnétique ---
-	plt.figure()
-	plt.subplot(1,2,1)
-	plt.errorbar(Rm[mask], MS_mean[mask], yerr=MS_mean_dist[mask], fmt='none', ecolor=colors1)
-	sc = plt.scatter(Rm[mask], MS_mean[mask], c=color_values, cmap=cmap1, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Reynolds magnetic")
-	plt.ylabel("MS mean")
-	plt.title("Radial mean of MS as a function of the Reynolds magnetic")
-	plt.grid()
-
-	plt.subplot(1,2,2)
-	plt.errorbar(Rm[mask], MS_max[mask], yerr=MS_max_dist[mask], fmt='none', ecolor=colors2)
-	sc = plt.scatter(Rm[mask], MS_max[mask], c=color_values, cmap=cmap2, norm=norm_discrete, zorder=5)
-	add_colorbar(sc)
-	plt.xlabel("Reynolds magnetic")
-	plt.ylabel("MS max")
-	plt.title("Radial max of MS as a function of the Reynolds magnetic")
-	plt.grid()
-	#plt.show()
-
-
 	x = Ro_conv[mask]
 	y = MS_mean[mask]
 	yerr = MS_mean_dist[mask]
 
-	def linear_model(x, a, b):
-		return a * x + b
-	    
-	def interp(x,y,yerr):
-		valid = (x > 0) & (y > 0)
-		x, y, yerr = x[valid], y[valid], yerr[valid]
-
-		logx = np.log10(x)
-		logy = np.log10(y)
-		logy_err = yerr / (y * np.log(10))
-
-		params, cov = curve_fit(linear_model, logx, logy, sigma=logy_err)
-		a, b = params
-
-		x_plot = np.logspace(np.log10(x.min()), np.log10(x.max()), 200)
-		y_plot = 10**b * x_plot**a
-		return(a,b,x_plot,y_plot)
-
 	# ======================== ROSSBY CONVECTIF =========================
 	a_mean,b_mean,x_plot,y_plot = interp(Ro_conv[mask],MS_mean[mask],MS_mean_dist[mask])
+	MS_obs = 10**b_mean * Ro_conv_obs**a_mean
+	
 	plt.figure()
 	plt.errorbar(Ro_conv[mask],MS_mean[mask], yerr=MS_mean_dist[mask], fmt='o')
+	plt.scatter(Ro_conv_obs, MS_obs, marker='*', s=120, color='red', edgecolor='black',label="Observations")
+	plt.fill_between(Ro_conv_obs,MS_obs_lim[0],MS_obs_lim[1],color = "gray")
 	plt.plot(x_plot, y_plot, color='black')
 	plt.xlabel("Convective Rossby")
 	plt.ylabel("MS mean")
@@ -488,13 +372,16 @@ for date, df in df_tot.groupby('date'):
 	a_max,b_max,x_plot,y_plot = interp(Ro_conv[mask],MS_max[mask],MS_max_dist[mask])
 	plt.figure()
 	plt.errorbar(Ro_conv[mask],MS_max[mask], yerr=MS_max_dist[mask], fmt='o')
+	plt.scatter(Ro_conv_obs, MS_obs, marker='*', s=120, color='red', edgecolor='black',label="Observations")
+	plt.fill_between(Ro_conv_obs,MS_obs_lim[0],MS_obs_lim[1],color = "gray")
 	plt.plot(x_plot, y_plot, color='black')
 	plt.xlabel("Convective Rossby")
 	plt.ylabel("MS max")
 	plt.title(rf"$MS_{{max}} = 10^{{{b_max:.2f}}} \cdot Ro_{{conv}}^{{{a_max:.2f}}}$")
 	plt.grid()
-	#plt.show()
+	plt.show()
 
+"""
 	# ======================== ELSASSER =========================
 	a_mean,b_mean,x_plot,y_plot = interp(Els[mask],MS_mean[mask],MS_mean_dist[mask])
 	plt.figure()
