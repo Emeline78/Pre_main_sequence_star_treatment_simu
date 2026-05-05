4f7de3e1c2a57c07fa7014ff7b65f36ad45942ac
localuser <localuser@e-m1-en-15>
2026-05-05 11:22:25 +0200

    modifications

--- scale_law1.py
+++ scale_law1.py
@@ -13,7 +13,10 @@ git push
 
 df = pd.read_parquet("transport_profiles.parquet")
 
-MS_mean = (df.groupby("name")["MS"].median()).to_numpy()
+#MS_mean = (df.groupby("name")["MS"].mean()).to_numpy()
+MS_rms = df.groupby("name").apply(lambda g: np.sqrt(np.mean(g["MS"]**2))).to_numpy()
+MS_int = df.groupby("name").apply(lambda g: np.trapz(g["MS"], g["r"])).to_numpy()
+
 #RS_mean = (df.groupby("name")["RS"].mean()).to_numpy()
 MS_max = df.groupby("name")["MS"].apply(lambda x: x.iloc[x.abs().argmax()]).to_numpy()
 #RS_max = (df.groupby("name")["RS"].max()).to_numpy()
@@ -33,8 +36,11 @@ Ro_sh = om*1e-4
 mask = (om < om_lim) & (df.groupby("name")["status"].first().to_numpy()) & (np.char.find(names, "wrong") == -1)
 #mask = (om > om_lim) & (df.groupby("name")["status"].first().to_numpy()) & (np.char.find(names, "wrong") == -1)
 
-MS_mean_dist = np.full(len(names),np.nan)
-MS_max_dist = np.full(len(names),np.nan)
+MS_mean_err = np.full(len(names),np.nan)
+MS_max_err = np.full(len(names),np.nan)
+MS_rms_err = np.full(len(names),np.nan)
+MS_int_err = np.full(len(names),np.nan)
+
 for i,namefile in enumerate(names): 
 	data = np.load("snapshots/"+namefile+".npz")
 	MS_snap = data["MS"]
@@ -42,13 +48,17 @@ for i,namefile in enumerate(names):
 	r = data["r"]
 	
 	x = np.mean(MS_snap, axis=1)
-	MS_snap_mean = np.trapz(x, times) / (times[-1]-times[0])
-	#print(MS_snap_mean,MS_mean[i])
-	MS_mean_dist[i] = np.sqrt(np.mean((x - MS_mean[i])**2)) / np.sqrt(len(x))
+	MS_mean_err[i] = np.std(x) / np.sqrt(len(x))
+	
+	x = np.sqrt(np.mean(MS_snap**2, axis=1))
+	MS_rms_err[i] = np.std(x) / np.sqrt(len(x))
+	
+	x = np.array([np.trapz(MS_snap[i], r) for i in range(len(MS_snap))])
+	MS_int_err[i] = np.std(x) / np.sqrt(len(x))
 	
 	x = MS_snap[np.arange(len(MS_snap)), np.abs(MS_snap).argmax(axis=1)]
 	#print(np.mean(x),MS_max[i])
-	MS_max_dist[i] = np.sqrt(np.mean((x - MS_max[i])**2)) / np.sqrt(len(x))
+	MS_max_err[i] = np.std(x) / np.sqrt(len(x))
 
 
 mask1 = mask & (g == 0)
@@ -57,19 +67,19 @@ mask3 = mask & (g == 2)
 g_labels = {0: r"$g \propto r$", 1: r"$g \propto 1/r^2$", 2: "CESAM 2k20"}
 plt.figure()
 plt.subplot(1,3,1)
-plt.errorbar(Ro_sh[mask1],MS_mean[mask1], yerr=MS_mean_dist[mask1], fmt='o',label = r"$g \propto r$")
+plt.errorbar(Ro_sh[mask1],MS_rms[mask1], yerr=MS_rms_err[mask1], fmt='o',label = r"$g \propto r$")
 plt.xlabel("Rossby shear")
 plt.ylabel("MS mean")
 plt.legend()
 
 plt.subplot(1,3,2)
-plt.errorbar(Ro_sh[mask2],MS_mean[mask2], yerr=MS_mean_dist[mask2], fmt='o', label = r"$g \propto 1/r^2$")
+plt.errorbar(Ro_sh[mask2],MS_rms[mask2], yerr=MS_rms_err[mask2], fmt='o', label = r"$g \propto 1/r^2$")
 plt.xlabel("Rossby shear")
 plt.ylabel("MS mean")
 plt.legend()
 
 plt.subplot(1,3,3)
-plt.errorbar(Ro_sh[mask3],MS_mean[mask3], yerr=MS_mean_dist[mask3], fmt='o', label = "CESAM 2k20")
+plt.errorbar(Ro_sh[mask3],MS_rms[mask3], yerr=MS_rms_err[mask3], fmt='o', label = "CESAM 2k20")
 plt.xlabel("Rossby shear")
 plt.ylabel("MS mean")
 plt.suptitle("Radial mean of MS as a function of the Rossby shear")
@@ -102,9 +112,9 @@ mask1 = mask & (xi == 0.1)
 mask2 = mask & (xi == 0.2)
 mask3 = mask & (xi == 0.35)
 plt.figure()
-plt.errorbar(Ro_conv[mask1],MS_mean[mask1], yerr=MS_mean_dist[mask1], fmt='o')
-plt.errorbar(Ro_conv[mask2],MS_mean[mask2], yerr=MS_mean_dist[mask2], fmt='o')
-plt.errorbar(Ro_conv[mask3],MS_mean[mask3], yerr=MS_mean_dist[mask3], fmt='o')
+plt.errorbar(Ro_conv[mask1],MS_rms[mask1], yerr=MS_rms_err[mask1], fmt='o')
+plt.errorbar(Ro_conv[mask2],MS_rms[mask2], yerr=MS_rms_err[mask2], fmt='o')
+plt.errorbar(Ro_conv[mask3],MS_rms[mask3], yerr=MS_rms_err[mask3], fmt='o')
 plt.xlabel("Convective Rossby")
 plt.ylabel("MS mean")
 plt.grid()
@@ -142,30 +152,22 @@ def interp(x,y,yerr):
 	y_plot = 10**b * x_plot**a
 	return(a,b,x_plot,y_plot)
 
-MS_sign = np.sign(MS_mean)
-MS_mean = np.abs(MS_mean)
+
 
 # ======================== ROSSBY CONVECTIF =========================
-a_mean,b_mean,x_plot,y_plot = interp(Ro_conv[mask],MS_mean[mask],MS_mean_dist[mask])
+a_mean,b_mean,x_plot,y_plot = interp(Ro_conv[mask],MS_rms[mask],MS_rms_err[mask])
 
 plt.figure()
 plt.subplot(1,2,1)
 plt.plot(x_plot, y_plot, color='red')
-plt.errorbar(Ro_conv[mask],MS_mean[mask], yerr=MS_mean_dist[mask], fmt='o')
+plt.errorbar(Ro_conv[mask],MS_rms[mask], yerr=MS_mean_dist[mask], fmt='o')
 plt.xlabel("Convective Rossby")
 plt.ylabel("MS mean")
 plt.grid()
 plt.title(rf"$MS_{{mean}} = 10^{{{b_mean:.2f}}} \cdot Ro_{{conv}}^{{{a_mean:.2f}}}$")
 
-plt.subplot(1,2,2)
-plt.plot(Ro_conv[mask], MS_sign[mask],"+")
-plt.xlabel("Convective Rossby")
-plt.ylabel("MS sign")
-plt.grid()
-plt.title("Sign of MS as a function of the Rossby")
-
 a_max,b_max,x_plot,y_plot = interp(Ro_conv[mask],MS_max[mask],MS_max_dist[mask])
-plt.figure()
+plt.subplot(1,2,2)
 plt.plot(x_plot, y_plot, color='red')
 plt.errorbar(Ro_conv[mask],MS_max[mask], yerr=MS_max_dist[mask], fmt='o')
 plt.xlabel("Convective Rossby")
@@ -175,10 +177,10 @@ plt.grid()
 
 
 # ======================== ELSASSER =========================
-a_mean,b_mean,x_plot,y_plot = interp(Els[mask],MS_mean[mask],MS_mean_dist[mask])
+a_mean,b_mean,x_plot,y_plot = interp(Els[mask],MS_rms[mask],MS_rms_err[mask])
 plt.figure()
 plt.subplot(1,2,1)
-plt.errorbar(Els[mask],MS_mean[mask], yerr=MS_mean_dist[mask], fmt='o')
+plt.errorbar(Els[mask],MS_rms[mask], yerr=MS_mean_dist[mask], fmt='o')
 plt.plot(x_plot, y_plot, color='red')
 plt.xlabel("Elsasser number")
 plt.ylabel("MS mean")
@@ -196,10 +198,10 @@ plt.grid()
 #plt.show()
 
 # ======================== RM =========================
-a_mean,b_mean,x_plot,y_plot = interp(Rm[mask],MS_mean[mask],MS_mean_dist[mask])
+a_mean,b_mean,x_plot,y_plot = interp(Rm[mask],MS_rms[mask],MS_rms_err[mask])
 plt.figure()
 plt.subplot(1,2,1)
-plt.errorbar(Rm[mask],MS_mean[mask], yerr=MS_mean_dist[mask], fmt='o')
+plt.errorbar(Rm[mask],MS_rms[mask], yerr=MS_mean_dist[mask], fmt='o')
 plt.plot(x_plot, y_plot, color='red')
 plt.xlabel("Reynolds magnetic")
 plt.ylabel("MS mean")
@@ -216,7 +218,7 @@ plt.title(rf"$MS_{{max}} = 10^{{{b_max:.2f}}} \cdot Rm^{{{a_max:.2f}}}$")
 plt.grid()
 plt.show()
 
-
+"""
 # ===================== OBSERVATIONAL DATA =====================
 
 B_obs_kG = np.array([1,3])   # kG
@@ -320,4 +322,4 @@ for date, df in df_tot.groupby('date'):
 		plt.grid()
 		plt.show()
 
-
+"""
