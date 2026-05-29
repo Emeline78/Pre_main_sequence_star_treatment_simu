7d5bbb15b8c80cffb0b280be2d1c9d9630bee791
localuser <localuser@e-m1-en-15>
2026-05-29 10:19:21 +0200

    modifications

--- scale_law_christensen.py
+++ scale_law_christensen.py
@@ -233,7 +233,7 @@ models = {"Ra_Q": [Ra_mod], "Ra_Q_Pm": [Ra_mod, Pm], "Ra_Q_Pm_Ro_sh": [Ra_mod, P
 
 for g_code in np.unique(g):
 
-	mask_g = mask & (g == g_code)
+	mask_g = mask #& (g == g_code)
 
 	npts = np.sum(mask_g)
 	print()
